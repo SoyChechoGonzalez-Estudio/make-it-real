eeb08efd64ca715133d37a4d59a8939bdf24c70d
Sergio González Sánchez <SoySergioGonzalez@Outlook.es>
2021-10-23 13:09:40 -0500

    Se añade condicionales en js

--- dia2/js/script.js
+++ dia2/js/script.js
@@ -1,1 +1,44 @@
-// Condicionales
+//Lógica Booleana y declaraciones if
+
+//console.log(2 < 1);
+//console.log(2 == 2);
+
+//let resultado = 5 > 4;
+//console.log(resultado);
+
+//let resultadoDoble = "42" == 42;
+//console.log(resultadoDoble);
+
+//let resultadoTriple = "42" === 42;
+//console.log(resultadoTriple);
+
+//let diferencia1 = "55" != 55;
+//console.log(diferencia1);
+
+//let diferencia2 = "55" !== 55;
+//console.log(diferencia2);
+
+//if (condición) {
+//console.log("enunciados a ejecutar si se cumple la condició");
+//} else {
+//console.log("enunciados a ejecutar si NO se cumple la condición");
+//}
+
+//if (variable_a_comparar > 100) {
+//alert("El valor de la variable es mayor que 100");
+//} else {
+//alert("El valor de la variable es menor o igual que 100");
+//}
+
+//if (condición) {
+//console.log("enunciados a ejecutar si se cumple la condición");
+//} else {
+//console.log("enunciados a ejecutar si NO se cumple la condición");
+//}
+
+const estado = 400;
+if (estado === 200) {
+  console.log("OK!");
+} else {
+  console.log("status desconocido");
+}
